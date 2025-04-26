dd0f691a4084a5bfff51bf529429c18954ac888d
Jason Bertman <jasondbertman@gmail.com>
2025-04-26 15:25:25 -0400

    Progress

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -1,6 +1,8 @@
 // TODO: Fix the compiler error without changing the function signature.
 fn current_favorite_color() -> String {
-    "blue"
+    // Both work
+    // String::from("blue")
+    "blue".to_string()
 }
 
 fn main() {
